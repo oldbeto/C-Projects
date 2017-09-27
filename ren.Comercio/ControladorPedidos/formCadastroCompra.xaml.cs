d24a4b439298e458571cdf2c54ba4543c9dd1ac1
Roberto Neto <Roberto.Neto@BR.Unisys.com>
2017-09-27 14:51:19 -0300

    Criacao de formulario de compras e cadastro de compras

---
+++ ren.Comercio/ControladorPedidos/formCadastroCompra.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ren.Comercio.Model;
+
+namespace ControladorPedidos
+{
+    /// <summary>
+    /// Interaction logic for formCadastroCompra.xaml
+    /// </summary>
+    public partial class formCadastroCompra : Window
+    {
+        RepositorioCompra repoCompra;
+
+        public formCadastroCompra()
+        {
+            InitializeComponent();
+            repoCompra = new RepositorioCompra();
+
+            this.DataContext = new Compra();
+        }
+
+        //Editar
+        public formCadastroCompra(Compra compra)
+        {
+            InitializeComponent();
+            repoCompra = new RepositorioCompra();
+
+            this.DataContext = compra;
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+            var compra = (Compra)this.DataContext;
+
+            if (lbCodigo.Content == null || lbCodigo.Content.ToString() == "0")
+            {
+                // novo
+                try
+                {
+                    repoCompra.Adicionar(compra);
+                    MessageBox.Show("Adicionado com Sucesso!", "Sucesso !", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Nao foi possivel Salvar!" + ex.Message, "Erro ao Salvar", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            else
+            {
+                // edicao
+                compra.codigo = Int32.Parse(lbCodigo.Content.ToString());
+                repoCompra.Atualizar(compra);
+                MessageBox.Show("Atualizado com Sucesso!", "Sucesso !", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            this.Close();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
+}
---
+++ ren.Comercio/ControladorPedidos/formCompras.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using ren.Comercio.Model;
+using ren.Comercio.Repositorio;
+
+namespace ControladorPedidos
+{
+    /// <summary>
+    /// Interaction logic for formCompras.xaml
+    /// </summary>
+    public partial class formCompras : Window
+    {
+        RepositorioCompra repositorio;
+
+        public formCompras()
+        {
+            InitializeComponent();
+            repositorio = new RepositorioCompra();
+        }
+
+        private void btMarcas_Click(object sender, RoutedEventArgs e)
+        {
+            var formMarca = new formMarcas();
+            formMarca.ShowDialog();
+        }
+
+        private void btEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (listProdutos.SelectedItem == null)
+            {
+                MessageBox.Show("Favor selecionar um item para edicao!", "Atencao", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                var compra = (Compra)listProdutos.SelectedItem;
+                var formCadastroProdutos = new formCadastroProdutos(compra);
+                formCadastroProdutos.ShowDialog();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CarregarLista();
+        }
+
+        private void btAtualizar_Click(object sender, RoutedEventArgs e)
+        {
+            CarregarLista();
+        }
+
+        private void CarregarLista()
+        {
+            listProdutos.DataContext = repositorio.Listar();
+        }
+
+        private void btNovo_Click(object sender, RoutedEventArgs e)
+        {
+            formCadastroProdutos formCadastro = new formCadastroProdutos();
+            formCadastro.ShowDialog();
+            CarregarLista();
+        }
+
+        private void btExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            if (listProdutos.SelectedItem == null)
+            {
+                MessageBox.Show("Favor selecionar um item para exclusao!", "Atencao", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (MessageBox.Show("Tem certeza que deseja excluir o item selecionado?", "Atencao", MessageBoxButton.YesNo, MessageBoxImage.Question).Equals(MessageBoxResult.Yes))
+                {
+                    var compra = (Compra)listProdutos.SelectedItem;
+                    repositorio.Excluir(compra);
+                }
+            }
+
+            CarregarLista();
+        }
+    }
+}
+}
